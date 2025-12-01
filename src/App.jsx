21183a3bd6ed292c16cea41a49ac14836234e943
Pratik-k-sahoo <sahoopratik5@gmail.com>
2025-12-01 21:18:39 +0530

    Pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router";
-import {Main, Auth, Admin, NotFound} from "@/pages";
+import {Main, Auth, Admin, NotFound, Transaction, Analytics} from "@/pages";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
@@ -13,6 +13,8 @@ function App() {
 				<Routes>
           <Route path="/auth" element={<Auth />} />
           <Route path="/" element={<Main />} />
+          <Route path="/transaction" element={<Transaction />} />
+          <Route path="/analytics" element={<Analytics />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,7 @@ import Admin from "./Admin";
 import Auth from "./Auth";
 import Main from "./Main";
 import NotFound from "./NotFound";
+import Transaction from "./Transaction";
+import Analytics from "./Analytics";
 
-export { Admin, Auth, Main, NotFound };
+export { Admin, Auth, Main, NotFound, Transaction, Analytics };
