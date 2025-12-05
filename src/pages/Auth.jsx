c5ef14ef09bfeedd299833a6fb6f6b495fe4d00a
Sushree-sucharita <pandasushreesucharita@gmail.com>
2025-12-05 21:26:38 +0530

    Login Page Created

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,23 +2,18 @@ import { useState, useEffect } from "react";
 import { toast } from "sonner";
 import { Wallet } from "lucide-react";
 import { useNavigate } from "react-router";
+import { Card } from "../components/ui/card";
+import {useForm, Controller} from "react-hook-form"
+import { Field, FieldGroup, FieldLabel } from "../components/ui/field";
+import { Input } from "@/components/ui/input"
 
 function Auth() {
 	const navigate = useNavigate();
 	const [loading, setLoading] = useState(false);
 	const user = null;
-
-	const [loginData, setLoginData] = useState({
-		email: "",
-		password: "",
-	});
-
-	const [signupData, setSignupData] = useState({
-		email: "",
-		password: "",
-		fullName: "",
-	});
-
+    const [signup, setSignup] = useState(false)
+	
+	
 	useEffect(() => {
 		if (user) {
 			navigate("/");
@@ -26,21 +21,7 @@ function Auth() {
 	}, [user, navigate]);
 
 	const handleLogin = async (e) => {
-		e.preventDefault();
-		setLoading(true);
-		let error = Math.random() * (100 - 1) + 1;
-		setTimeout(() => {
-			if (error % 2 === 0) console.log(loginData.email, loginData.password);
-			else error = "Error Occurred while logging in.";
-		}, 5000);
-
-		if (error) {
-			toast.error(error.message);
-		} else {
-			toast.success("Welcome back!");
-			navigate("/");
-		}
-		setLoading(false);
+		console.log(e)
 	};
 
 	const handleSignup = async (e) => {
@@ -61,13 +42,62 @@ function Auth() {
 		}
 		setLoading(false);
 	};
-
+    const form = useForm({
+		defaultValues: {
+		 fName:"",
+		 email:"",
+		 password:"",
+		}
+	})
 	return (
-		<div>
-			<h1>Login Signin Page</h1>
-			<h2>Then logout</h2>
-      <button onClick={handleLogin}>Login Sushree</button>
-      <button onClick={handleSignup}>Signin</button>
+		<div className="min-h-screen bg-linear-to-br from-primary to-accent  flex items-center justify-center">
+			<main className="container px-4 py-8 space-y-6 flex justify-center items-center">
+                <Card className="w-3xl flex flex-col items-center justify-center gap-14 p-6">
+					<span className="text-4xl font-medium tracking-wider bg-linear-to-r from-primary to-accent bg-clip-text text-transparent p-2">
+						Login Page
+					</span>
+					<div className="w-3/4 tracking-wide">
+					<form id="login-form" onSubmit={form.handleSubmit(handleLogin)}>
+                        <FieldGroup>
+							{signup && (
+								<Controller name="fName" control={form.control} render={({field, fieldState})=>(
+									<Field data-invalid={fieldState.invalid}>
+										<FieldLabel className="text-lg" htmlFor="fName">
+											FullName:
+										</FieldLabel>
+                                        <Input {...field} className="w-full" id="fName" aria-invalid={fieldState.invalid} placeholder="Enter Your FullName" />
+									</Field>
+								)}
+
+								/>
+							)}
+
+							<Controller name="email" control={form.control} render={({field, fieldState})=>(
+									<Field data-invalid={fieldState.invalid}>
+										<FieldLabel className="text-lg" htmlFor="email">
+											Email:
+										</FieldLabel>
+                                        <Input {...field} className="w-full" id="email" aria-invalid={fieldState.invalid} placeholder="Enter Your Email" />
+									</Field>
+								)}
+
+								/>
+								<Controller name="password" control={form.control} render={({field, fieldState})=>(
+									<Field data-invalid={fieldState.invalid}>
+										<FieldLabel className="text-lg" htmlFor="password">
+											Password:
+										</FieldLabel>
+                                        <Input {...field} className="w-full" id="password" aria-invalid={fieldState.invalid} placeholder="Enter Your Password" />
+									</Field>
+								)}
+
+								/>
+						</FieldGroup>
+					</form>
+					</div>
+
+				</Card>
+			</main>
 		</div>
 	);
 
